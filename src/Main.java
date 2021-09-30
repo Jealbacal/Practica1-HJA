0e7f1b21c00ecb32d911e2088a27e73b5c8f3d95
Jealbacal <72936190+Jealbacal@users.noreply.github.com>
2021-09-30 15:35:09 +0200

    evalúa de ap2 listo y atributos en mano

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+package Main;
+
+import java.util.*;
+
+import Ap1.ap1;
+import Ap2.ap2;
+
+public class Main {
+
+	
+	public static void main ( String [] args) {
+		String prc, in, out;
+		prc = new String(args[0]);
+		in = new String(args[1]);
+		out = new String(args[2]);
+		
+		switch(prc) {
+		case "1":
+			ap1 a1 = new ap1(in, out);
+			break;
+		case "2":
+			ap2 a2 = new ap2(in, out);
+			break;
+		case "3":
+			//ap3 a3 = new ap3(in, out);
+			break;
+		default:
+			//error
+		}
+	}
+}
